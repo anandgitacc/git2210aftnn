fdcbcadae662d44bbd610847f6b2f9611507cbfe
anandgitacc <anand.akmail@gmail.com>
2021-10-22 17:03:24 +0530

    testan

---
+++ src/org/testgit/Testgitaftnn.java
@@ -0,0 +1,22 @@
+package org.testgit;
+
+
+public class Testgitaftnn {
+	
+	public void empName() {
+		System.out.println("Employee name is Anand");
+	}
+	
+	public void empMobno() {
+		System.out.println("Employee mobile no is 8667070832");
+	}
+	
+	public static void main(String[] args) {
+		
+		Testgitaftnn a = new Testgitaftnn();
+		
+		a.empName();
+		a.empMobno();
+		
+	}
+}
